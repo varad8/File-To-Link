0904d9415800435f31ed4ae50c2ed8a153efe425
Varad Nikharage <78202186+varad8@users.noreply.github.com>
2023-11-18 18:13:07 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
+    DATABASE_URL = str(getenv('mongodb+srv://silentmind2002:q0RWVLHinTylQls6@cluster0.hnre9cx.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('@vstreamupdate', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
