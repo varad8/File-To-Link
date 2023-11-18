4fc85e02cd06e7847e877681e6eef71637aa1f6a
Varad Nikharage <78202186+varad8@users.noreply.github.com>
2023-11-18 17:14:03 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -19,7 +19,7 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1812010571").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = str(getenv('APP_NAME'))
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
